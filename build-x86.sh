181fc3ab1817a614e9393a0e595d81e0e4c3c748
Igor Liferenko <igor.liferenko@gmail.com>
2017-10-08 17:38:00 +0700

    edit

--- build-x86.sh
+++ build-x86.sh
@@ -31,16 +31,16 @@ cat << EOF > files/etc/uci-defaults/my
 uci set network.lan.proto=dhcp
 uci commit network
 EOF
-make info; exit # make image PROFILE=???? PACKAGES="nfs-utils kmod-fs-nfs kmod-usb-hid kmod-hid-generic kmod-usb-ohci" FILES=files/
+make image PROFILE=Generic PACKAGES="nfs-utils kmod-fs-nfs kmod-usb-hid kmod-hid-generic kmod-usb-ohci" FILES=files/
 gunzip bin/x86/openwrt-x86-64-combined-ext4.img.gz
 rm -fr /var/local/x86/
 mkdir /var/local/x86/
 # copy sdk:
-tar -C /var/local/x86 -jxf bin/x86/OpenWrt-SDK-*.tar.bz2
+tar -C /var/local/x86 -jxf ../../$SDK.tar.bz2
 rm -f /var/local/x86-sdk
 ln -s /var/local/x86/*/staging_dir/toolchain* /var/local/x86-sdk
 rm -f /usr/local/SUPER_DEBIAN/x86-sdk.tar.bz2
-cp bin/x86/OpenWrt-SDK-*.tar.bz2 /usr/local/SUPER_DEBIAN/x86-sdk.tar.bz2
+cp ../../$SDK.tar.bz2 /usr/local/SUPER_DEBIAN/x86-sdk.tar.bz2
 # copy img:
 cp bin/x86/openwrt-x86-64-combined-ext4.img /var/local/x86/x86.img
 rm -f /usr/local/SUPER_DEBIAN/x86.img
