57ac026e81d73591b5c2e261306fe0b724da7c7f
Igor Liferenko <igor.liferenko@gmail.com>
2018-05-12 16:17:45 +0700

    edit

--- build-snapshot.sh
+++ build-snapshot.sh
@@ -25,7 +25,7 @@ uci commit network
 uci set system.@system[0].timezone=GMT-7
 uci commit system
 EOF
-make image PROFILE=Generic PACKAGES="nfs-utils kmod-fs-nfs kmod-usb-ohci kmod-usb-acm kmod-usb-hid kmod-hid-generic kmod-usb-serial-ftdi mpc" FILES=files/
+make image PROFILE=Generic PACKAGES="nfs-utils kmod-fs-nfs kmod-usb-uhci kmod-usb-acm" FILES=files/ # NOTE: if it will not work, add kmod-usb-ehci
 gunzip bin/targets/x86/64/openwrt-x86-64-combined-ext4.img.gz
 rm -fr /var/local/x86/
 mkdir /var/local/x86/
