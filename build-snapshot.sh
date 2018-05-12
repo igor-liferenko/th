e83431a5244bfdec4cf200e32b85fab9dd5362ee
Igor Liferenko <igor.liferenko@gmail.com>
2018-05-12 15:50:42 +0700

    fix

--- build-snapshot.sh
+++ build-snapshot.sh
@@ -25,8 +25,7 @@ uci commit network
 uci set system.@system[0].timezone=GMT-7
 uci commit system
 EOF
-make image PROFILE=Generic PACKAGES="nfs-utils kmod-fs-nfs kmod-usb-ohci kmod-usb-acm kmod-usb-hid kmod-hid-generic kmod-usb-serial-ftdi mpc
-" FILES=files/
+make image PROFILE=Generic PACKAGES="nfs-utils kmod-fs-nfs kmod-usb-ohci kmod-usb-acm kmod-usb-hid kmod-hid-generic kmod-usb-serial-ftdi mpc" FILES=files/
 gunzip bin/targets/x86/64/openwrt-x86-64-combined-ext4.img.gz
 rm -fr /var/local/x86/
 mkdir /var/local/x86/
