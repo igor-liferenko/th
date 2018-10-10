a12d0e312ef0ad45e4abbf1efac1f886f6a0871a
Igor Liferenko <igor.liferenko@gmail.com>
2018-10-10 16:28:34 +0700

    it is not used anymore

--- devtag.h
+++
@@ -1,7 +0,0 @@
-/* Copyright 2010 Stefan Tomanek <stefan.tomanek+th@wertarbyte.de>
- * You have permission to copy, modify, and redistribute under the
- * terms of the GPLv3 or any later version.
- * For full license terms, see COPYING.
- */
-
-#define TH_DEVICE_TAG_LENGTH 256
--- version.h
+++
@@ -1,1 +0,0 @@
-#define TH_VERSION "0.3.4"
